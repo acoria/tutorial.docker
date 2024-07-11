7545d3e82ccac8ea0b4c69ec41b13b06a8ca56ed
vtewes <verena.tewes@gmx.net>
2024-07-11 16:01:33 +0200

    Add how to run a container

--- express_server/src/app.ts
+++ express_server/src/app.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const PORT = 5000;
 const server = express();
-server.get("/test", (_, res) => {
-  res.status(200).send("Test successful");
+server.get("/", (_, res) => {
+  res.status(200).send("Welcome!");
 });
 
 server.listen(PORT, () => console.log("Server listening to port " + PORT));
